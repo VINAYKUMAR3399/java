4cfc33db1c62e212c80c34c0b04d584bfab31ab6
VINAYKUMAR3399 <vinayas.xworkz@gmail.com>
2024-06-24 12:56:38 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,15 @@
+public class Add{
+ 
+ public static void main(String args[])
+ 
+ {
+ int num = 89;
+ int num1 = 3;
+ int sum = num+num1;
+ if(num<=sum-3){
+	 System.out.println("The number is equal to first number");
+ }else
+	 System.out.println("The number is not equal to first number");
+ }
+
+}
