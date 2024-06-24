1cd495d77dcf6c697d3256794e39f93401023be2
VINAYKUMAR3399 <vinayas.xworkz@gmail.com>
2024-06-24 12:59:46 +0530

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,9 @@
+public class Course{
+public static void main(String args[]){
+int fees = 12000;
+int gst = 18;
+double totalfees=((12000*(18/100))+(12000));
+System.out.println("The total course fees is:"+totalfees);
+
+}                                            
+}
---
+++ SubjectRunner.java
@@ -0,0 +1,14 @@
+public class SubjectRunner
+{
+	public static void main(String[] args) 
+	{
+	    int telugu = 99;
+	    int english = 98;
+	    int hindi = 99;
+	    int science = 88;
+	    int maths = 96;
+	    int zoology = 73;
+	   int total = telugu+english+hindi+science+maths+zoology;
+		System.out.println("The Total marks of a student: "+total);
+	}
+	}
