d6d6cf914a063fc5b032cc855ddad00cabe014cf
VINAYKUMAR3399 <vinayas.xworkz@gmail.com>
2024-06-24 13:05:11 +0530

    this is my second commit

---
+++ Contact.java
@@ -0,0 +1,16 @@
+public class Contact{
+	public static void main(String args[]){
+	
+	long contact = 9281570063L;
+	long landline=8328084622L;
+	if(contact>landline){
+		System.out.println("contact is greater");
+		
+	}
+	else{
+		System.out.println("contact is not greater");
+	}
+	}
+	
+	
+	}
